cb029097fd15b6e3f1275fac11e6a45a66e5c344
xiaohu <duonght2004.itsgu@gmail.com>
2025-11-04 20:52:00 +0700

    feat(product):add dto request update ,filter

---
+++ Modules/Products/Dtos/Request/UpdateCategoryRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.API.Modules.Products.Dtos.Request
+{
+    public class UpdateCategoryRequest
+    {
+        [Required(ErrorMessage = "Tên danh mục là bắt buộc.")]
+        [StringLength(100, ErrorMessage = "Tên danh mục không được vượt quá 100 ký tự.")]
+        public string CategoryName { get; set; } = string.Empty;
+
+      
+        [Required(ErrorMessage = "Mã code danh mục là bắt buộc.")]
+        [StringLength(50, ErrorMessage = "Mã code không được vượt quá 50 ký tự.")]
+        public string CategoryCode { get; set; } = string.Empty;
+    }
+}
